f50d5d96476cedd74e7e4aa897dd3829e7c89424
B Mathis <brandon@imathis.com>
2010-04-24 00:57:24 -0500

    commented out install conditionals, they were breaking the file section of the gemspec, also they were not working correctly anyway

--- lib/templates/project/manifest.rb
+++ lib/templates/project/manifest.rb
@@ -2,10 +2,10 @@
 stylesheet 'ie6.sass', :media => 'screen, projection', :condition => "lt IE 7"
 image 'button_bg.png'
 
-print "Install example files? (html and buttons.sass) (Y/n)?" 
-response = gets
-if response.downcase == "y" || response.downcase == "\n"
-  file 'index.html'
-  stylesheet 'buttons.sass', :media => 'screen, projection'
-end
-print 'Don\'t forget to @import "fancy-buttons"'
+#print "Install example files? (html and buttons.sass) (Y/n)?" 
+#response = gets
+#if response.downcase == "y" || response.downcase == "\n"
+file 'index.html'
+stylesheet 'buttons.sass', :media => 'screen, projection'
+#end
+#print 'Don\'t forget to @import "fancy-buttons"'
